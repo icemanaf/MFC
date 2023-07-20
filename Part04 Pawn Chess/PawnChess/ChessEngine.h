97c35ae34f3e2d05ad519f6673127765585416af
James Grunewald <james.grunewald@slalom.com>
2023-07-20 17:11:29 +0100

    (WIP) ChessEngine and tests

--- Part04 Pawn Chess/PawnChess/ChessEngine.h
+++ Part04 Pawn Chess/PawnChess/ChessEngine.h
@@ -2,9 +2,8 @@
 
 struct ChessBoard
 {
-	 uint64_t WhitePawns;
-
-	 uint64_t BlackPawns;
+    uint64_t BlackPawns; // Formerly SystemPawns
+	uint64_t WhitePawns; // Formerly UserPawns
 };
 
 
@@ -14,4 +13,15 @@ enum MOVE_STATUS
     MOVE_ILLEGAL,
     USER_WINS,
     SYSTEM_WINS
+};
+
+static class PawnChessEngine
+{
+public:
+    //const int32_t INFINITY = 2147483647;
+    static ChessBoard ReplyMove;
+
+    static bool IsPositionLegal(ChessBoard currentPos);
+
+
 };
